c689bcf5e32c6641ab85772a8f964d12402bcc44
vojtisek2 <vojtisek2@spsejecna.cz>
2024-06-01 10:29:17 +0200

    text

--- pcss/footer.php
+++ pcss/footer.php
@@ -1,4 +1,4 @@
-<footer class="bg-body-tertiary text-center text-lg-start">
+/<footer class="bg-body-tertiary text-center text-lg-start">
 
     <div class="container p-4">
 
@@ -7,9 +7,7 @@
             <div class="col-lg-6 col-md-12 mb-4 mb-md-0">
                 <h5 class="text-uppercase">Footer text</h5>
                 <p>
-                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iste atque ea quis
-                    molestias. Fugiat pariatur maxime quis culpa corporis vitae repudiandae aliquam
-                    voluptatem veniam, est atque cumque eum delectus sint!
+                    Hadry mega hadry v našem sekači se vešchno najde 
                 </p>
             </div>
             <div class="col-lg-6 col-md-12 mb-4 mb-md-0">
